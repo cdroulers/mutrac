e106ad4f62a85ae9f206873d429b3ec61857a95e
cdroulers <droulers.christian@gmail.com>
2016-02-23 09:28:37 -0500

    Clean master. will go the simple route first.

--- src/app/index.tsx
+++
@@ -1,8 +0,0 @@
-/* tslint:disable:no-unused-variable */
-require("./main.less");
-import React = require("react");
-/* tslint:disable:no-unused-variable */
-import ReactDOM = require("react-dom");
-
-const app = <div>oh hai</div>;
-ReactDOM.render(app, document.getElementById("root"));
